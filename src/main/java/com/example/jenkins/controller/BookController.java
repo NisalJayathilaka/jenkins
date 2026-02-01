30553d0ff1a483f515b5778141c4cf228eeba152
nisal <nisal@dmsswt.com>
2026-02-01 18:21:44 +0530

    weebhooks test

--- src/main/java/com/example/jenkins/controller/BookController.java
+++ src/main/java/com/example/jenkins/controller/BookController.java
@@ -24,7 +24,7 @@ public class BookController {
     @GetMapping("/all")
     public List<Book> getAllBooks(){
         List<Book> bookList = bookService.getAllBooks();
-        String a  = "nisal";
+        String a  = "nisalasass";
         return bookList;
     }
 }
